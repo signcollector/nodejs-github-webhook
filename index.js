64a9d326ce2c94923750c6ad350f87afd1d63654
Ueli Kunz <kunz@ideadapt.net>
2018-12-25 18:31:31 +0100

    update to curruent node

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
+const fs = require('fs');
 var http    = require('http');
 var spawn   = require('child_process').spawn;
 var crypto  = require('crypto');
 var url     = require('url');
 
-var secret  = 'amazingkey'; // secret key of the webhook
+var secret  = 'blastingyourmindawaywithit'; // secret key of the webhook
 var port    = 8081; // port
 
+const out = fs.openSync('./out.log', 'a');
+const err = fs.openSync('./err.log', 'a');
+
 http.createServer(function(req, res){
-    
+
     console.log("request received");
     res.writeHead(400, {"Content-Type": "application/json"});
 
     var path = url.parse(req.url).pathname;
 
-    if(path!='/push' || req.method != 'POST'){
-       var data = JSON.stringify({"error": "invalid request"});
-       return res.end(data); 
+    if(req.method != 'POST'){
+       var data = JSON.stringify({"error": "invalid request, expecting POST"});
+       return res.end(data);
     }
-
 
     var jsonString = '';
     req.on('data', function(data){
@@ -30,25 +33,16 @@ http.createServer(function(req, res){
           console.log('invalid key');
           var data = JSON.stringify({"error": "invalid key", key: hash});
           return res.end(data);
-      } 
-       
+      }
+
       console.log("running hook.sh");
-   
-      var deploySh = spawn('sh', ['hook.sh']);
-      deploySh.stdout.on('data', function(data){
-          var buff = new Buffer(data);
-          console.log(buff.toString('utf-8'));
-      });
 
-      
-    res.writeHead(400, {"Content-Type": "application/json"});
-    
-    var data = JSON.stringify({"success": true});
+      var deploySh = spawn('sh', ['hook.sh'], {detached: true, stdio: ['ignore', out, err]});
+      deploySh.unref();
+
+      res.writeHead(200, {"Content-Type": "application/json"});
+
+      var data = JSON.stringify({"success": true});
       return res.end(data);
- 
     });
-
-    
 }).listen(port);
-
-console.log("Server listening at " + port);
