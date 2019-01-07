6a4bf5ef2791793c46210af1a461d36015fd8b1a
Ueli Kunz <kunz@ideadapt.net>
2019-01-07 17:47:02 +0100

    wait for hook.sh to finish. respond according to process exit code.

--- index.js
+++ index.js
@@ -1,48 +1,53 @@
 const fs = require('fs');
-var http    = require('http');
-var spawn   = require('child_process').spawn;
-var crypto  = require('crypto');
-var url     = require('url');
+const http    = require('http');
+const spawn   = require('child_process').spawn;
+const crypto  = require('crypto');
 
-var secret  = 'blastingyourmindawaywithit'; // secret key of the webhook
-var port    = 8081; // port
+const webhookSecret  = 'blastingyourmindawaywithit';
+const port    = 8081;
 
-const out = fs.openSync('./out.log', 'a');
-const err = fs.openSync('./err.log', 'a');
+const contenType = {"Content-Type": "application/json"};
+const dateName = new Date().toISOString().replace(/\:/g, "-").replace(/\./g, "-");
+const errFile = './'+dateName+'-err.log';
+const outFile = './'+dateName+'-out.log';
+const out = fs.openSync(outFile, 'a');
+const err = fs.openSync(errFile, 'a');
 
 http.createServer(function(req, res){
-
     console.log("request received");
-    res.writeHead(400, {"Content-Type": "application/json"});
-
-    var path = url.parse(req.url).pathname;
+    res.writeHead(400, contenType);
 
     if(req.method != 'POST'){
        var data = JSON.stringify({"error": "invalid request, expecting POST"});
        return res.end(data);
     }
 
-    var jsonString = '';
+    const jsonString = '';
     req.on('data', function(data){
         jsonString += data;
     });
 
     req.on('end', function(){
-      var hash = "sha1=" + crypto.createHmac('sha1', secret).update(jsonString).digest('hex');
+      const hash = "sha1=" + crypto.createHmac('sha1', webhookSecret).update(jsonString).digest('hex');
       if(hash != req.headers['x-hub-signature']){
           console.log('invalid key');
-          var data = JSON.stringify({"error": "invalid key", key: hash});
+          var data = JSON.stringify({success: false, msg: "invalid key", key: hash});
           return res.end(data);
       }
 
       console.log("running hook.sh");
 
       var deploySh = spawn('sh', ['hook.sh'], {detached: true, stdio: ['ignore', out, err]});
-      deploySh.unref();
+      deploySh.on('close', (code) => {
+        if(code !== 0){
+            res.writeHead(500, contenType);   
+            res.end(JSON.stringify({success: false, msg: fs.readFileSync(errFile), code}));
+        }
 
-      res.writeHead(200, {"Content-Type": "application/json"});
+        res.writeHead(200, contenType);
 
-      var data = JSON.stringify({"success": true});
-      return res.end(data);
+        const data = JSON.stringify({success: true});
+        return res.end(data);
+      });
     });
 }).listen(port);
