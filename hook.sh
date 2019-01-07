6818df319f2064be0c498c7bae5a64b1e84751be
Ueli Kunz <kunz@ideadapt.net>
2019-01-07 17:51:32 +0100

    apply hook.sh changes locally

--- hook.sh
+++ hook.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# DIRECTORY TO THE REPOSITORY
-REPOSITORY="../repo"
-
-cd $REPOSITORY
-
-git pull
+git_pull_digiges_collector() {
+  ssh-agent bash -c 'ssh-add /home/runner/.ssh/deploy; git pull origin master'
+}
+cd ~/deploy/digiges-collector/deployment/
+git_pull_digiges_collector
+./deploy.sh staging
