097752c5896c6e520536ab0a8e5cc5f39c11df9b
Ueli Kunz <kunz@ideadapt.net>
2019-09-12 00:11:59 +0200

    fix path :)

--- hook.sh
+++ hook.sh
@@ -3,7 +3,7 @@
 git_pull_signcollector() {
   ssh-agent bash -c 'ssh-add /home/runner/.ssh/deploy; git pull origin master'
 }
-cd ~/deploy/signcollector/deployment/
+cd ~/deploy/signcollector-application/deployment/
 git_pull_signcollector
 
 ./deploy.sh staging --with-db
