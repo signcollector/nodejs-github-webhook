29d860547cc9e0d5c536a1a09aca8ccf15bc1875
Ueli Kunz <kunz@ideadapt.net>
2019-09-11 22:48:35 +0200

    signcollector renaming

--- hook.sh
+++ hook.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git_pull_digiges_collector() {
+git_pull_signcollector() {
   ssh-agent bash -c 'ssh-add /home/runner/.ssh/deploy; git pull origin master'
 }
-cd ~/deploy/digiges-collector/deployment/
-git_pull_digiges_collector
+cd ~/deploy/signcollector/deployment/
+git_pull_signcollector
 ./deploy.sh staging
