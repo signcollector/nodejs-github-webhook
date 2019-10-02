cdbb1dc071848718e27cf80c01f6eec0bb937d68
Ueli Kunz <kunz@ideadapt.net>
2019-10-02 23:09:30 +0200

    always clone and cd

--- deploy_hook.sh
+++ deploy_hook.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
+set -e
 
 test -z "$1" && echo 'env name missing' && exit 1
 
+git_pull_signcollector() {
+  ssh-agent bash -c 'ssh-add /home/runner/.ssh/deploy; git pull origin master'
+}
+
 ENV_NAME=$1
 
-if [[ $ENV_NAME == 'staging' ]]; then
-  git_pull_signcollector() {
-    ssh-agent bash -c 'ssh-add /home/runner/.ssh/deploy; git pull origin master'
-  }
-  cd ~/deploy/signcollector-application/deployment/ || exit
-  git_pull_signcollector
+cd ~/deploy/signcollector-application/deployment/ || exit
+git_pull_signcollector
 
+if [[ "$ENV_NAME" == 'staging' ]]; then
+  # complete redeploy of staging env
   ./deploy.sh staging --with-db
-
 else
   # update configurations of a running campaign
   ./merge-deployment-configuration.sh $ENV_NAME --skip-credentials
