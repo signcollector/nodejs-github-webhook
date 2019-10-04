e5a60b31ab7043d57eef93e7228d1c11dcdb2e2e
Ueli Kunz <kunz@ideadapt.net>
2019-10-04 09:47:13 +0200

    add timestamp to log

--- deploy_hook.sh
+++ deploy_hook.sh
@@ -7,9 +7,13 @@ git_pull_signcollector() {
   ssh-agent bash -c 'ssh-add /home/runner/.ssh/deploy; git pull origin master'
 }
 
+echo_time() {
+    date +"%Y-%m-%dT%H:%M:%S $(printf "%s " "$@" | sed 's/%/%%/g')"
+}
+
 ENV_NAME=$1
 
-printf "using env $ENV_NAME\n"
+echo_time "using env $ENV_NAME"
 
 cd ~/deploy/signcollector-application/deployment/ || exit
 git_pull_signcollector
